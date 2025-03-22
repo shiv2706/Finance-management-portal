21cf779f807d690a21119747554a95b97b932d18
Shivansh Pradhan <shivansh4858@gmail.com>
2025-03-22 23:38:17 +0530

    password hashing implemented

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -16,6 +17,13 @@ const userSchema = new mongoose.Schema({
     }
 
 },{timestamps:true});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {return next()}
+
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+})
 
 const userModel = mongoose.model('User', userSchema);
 module.exports = userModel;
